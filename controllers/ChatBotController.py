9a06a586bded5494e1321fcb3cb25e41772f5258
YoNoSoyMarinero1 <1milosmarinkovic1@gmail.com>
2023-10-30 13:47:06 +0100

    Add numbers to words preprocessing

--- controllers/ChatBotController.py
+++ controllers/ChatBotController.py
@@ -1,8 +1,9 @@
 from schemas.UserMessageSchema import UserMessage
 from services.GPTCommunicationService import GPTCommunicationService
+from services.MessagePreprocessingService import MessagePreprocessingPipelineService
 
 class ChatBotController:
     @staticmethod
     def send_message_to_chatbot(user_message: UserMessage) -> dict:
-        #Using service to provide response
+        user_message_text_preprocessed: str = MessagePreprocessingPipelineService.preprocess_message_pipeline(user_message.message_text)
         return GPTCommunicationService.send_message_to_gpt(user_message.message_text)
